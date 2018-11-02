87373885362e614bc751c1b3e2d7b7a81e1e3b34
Ben Bussell <benbussell@gmail.com>
2018-11-02 13:34:50 -0400

    Add project files.

---
+++ TranslateToMorse/MorseConverter.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+namespace TranslateToMorse
+{
+    static class MorseConverter
+    {
+        private static Dictionary<char, string> _letterCode = new Dictionary<char, string>
+        {
+            {' ', "/"},
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."}, 
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."},
+            {'0', "-----"},
+            {',', "--..--"},
+            {'.', ".-.-.-"},
+            {'?', "..--.."},
+            {';', "-.-.-."},
+            {':', "---..."},
+            {'/', "-..-."},
+            {'-', "-....-"},
+            {'\'', ".----."},
+            {'"', ".-..-."},
+            {'(', "-.--."},
+            {')', "-.--.-"},
+            {'=', "-...-"},
+            {'+', ".-.-."},
+            {'@', ".--.-."},
+            {'!', "-.-.--"},
+            {'Á', ".--.-"},
+            {'É', "..-.."},
+            {'Ö', "---."},
+            {'Ä', ".-.-"},
+            {'Ñ', "--.--"},
+            {'Ü', "..--"}
+            };
+
+        public static string ToMorseCode (string input)
+        {
+            List<string> translated = new List<string>(input.Length);
+            
+                foreach (char letter in input.ToUpper())
+                {
+                    try
+                    {
+                        string morseCode = _letterCode[letter];
+                        translated.Add(morseCode);
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                    translated.Add("key unavailable");
+                    }
+                }
+            var result =  string.Join(" ", translated);
+            return result;
+        }
+
+        public static char[] PlayMessage(string bInput)
+        {
+            string morseText = ToMorseCode(bInput);
+            char[] dotNDash = bInput.ToCharArray();
+            return dotNDash;
+            foreach(char character in dotNDash)
+            {
+
+            }
+        }
+    }
+}
---
+++ TranslateToMorse/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace TranslateToMorse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string userChoice = string.Empty;
+            while (userChoice != "5")
+            {
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("    Welcome to the Morse Translator!");
+                Console.WriteLine("       What would you like to do?");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("1. Translate a word or phrase");
+                Console.WriteLine("2. View a list of common phrases");
+                Console.WriteLine("3. Add a new word or phrase to the list");
+                Console.WriteLine("4. Remove a word or phrase from the list");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("------------------------------------------");
+            
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+
+                        while (true)
+                        {
+                            Console.Write("Please enter a word or phrase to translate:  ");
+                            string input = Console.ReadLine();
+
+                            if (string.IsNullOrWhiteSpace(input))
+                            {
+                                break;
+                            }
+
+                            string translate = MorseConverter.ToMorseCode(input);
+
+                            Console.WriteLine(translate);
+
+                            string bString = string.Empty;
+                            while (bString != "q" && bString != "Q")
+                            {
+                                Console.WriteLine("Enter 'B' to hear the code or enter 'Q' to exit to main menu");
+                                string bString = Console.ReadLine();
+
+                                    char[] dotNDash = Beeps.PlayMessage(translate);
+                                    Console.WriteLine(dotNDash[]);
+                               
+                            }
+
+
+
+
+
+
+                        }
+
+                        break;
+
+                    case "2":
+
+                        string currentDirectory = Directory.GetCurrentDirectory();
+                        DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+                        var fileName = Path.Combine(directory.FullName, "phrases.txt");
+                        var file = new FileInfo(fileName);
+                        if (file.Exists)
+                        {
+                            using (var reader = new StreamReader(file.FullName))
+                            {
+                                Console.SetIn(reader);
+                                Console.WriteLine(Console.ReadLine());
+                            }
+
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Case 3");
+                        break;
+                    case "4":
+                        Console.WriteLine("Case 4");
+                        break;
+                    case "5":
+
+                        break;
+                    default:
+                        Console.WriteLine("Please Try Again");
+                }
+            
+        }
+    }
+}
